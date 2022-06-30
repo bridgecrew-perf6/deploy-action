ee76bb3b74285e024d7e2333a759165d8104afef
Eero Paukkonen <eero.paukkonen@oda.com>
2022-06-30 06:45:58 +0300

    Run prettier

--- dist/index.js
+++ dist/index.js
@@ -9002,42 +9002,41 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
-function getBranch(){
+function getBranch() {
   const REFS_HEADS = "refs/heads/";
   const headRef = process.env.GITHUB_HEAD_REF;
   let branch = headRef || github.context.ref;
-  if(branch && branch.indexOf(REFS_HEADS) === 0){
-    branch = branch.substring(REFS_HEADS.length)
+  if (branch && branch.indexOf(REFS_HEADS) === 0) {
+    branch = branch.substring(REFS_HEADS.length);
   }
   return branch;
 }
 
-function getSha(){
+function getSha() {
   const sha = github.context.sha;
-  if(!sha){
+  if (!sha) {
     throw Error(`Invalid sha ${sha}`);
   }
   return sha;
 }
 
 try {
-  const token = core.getInput('github-token', {required: true});
+  const token = core.getInput("github-token", { required: true });
 
-  const environment = core.getInput('environment', {required: true});
+  const environment = core.getInput("environment", { required: true });
   const branch = getBranch();
   const sha = getSha();
 
   const owner = github.context.repo.owner;
   const repo = github.context.repo.repo;
 
-  
   console.log(`environment ${environment}`);
   console.log(`branch ${branch}`);
   console.log(`sha ${sha}`);
   console.log(`owner ${owner}`);
   console.log(`repo ${repo}`);
 
-  const octokit = github.getOctokit(token)
+  const octokit = github.getOctokit(token);
 
   octokit.rest.repos.createDeployment({
     owner,
@@ -9047,13 +9046,15 @@ try {
     environment,
     auto_merge: false,
     payload: {
-      branch
+      branch,
     },
-  })
+  });
 
+  console.log("Github Deployment created");
 } catch (error) {
   core.setFailed(`Error: Failed to create deployment: ${error.message}`);
 }
+
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-function getBranch(){
+function getBranch() {
   const REFS_HEADS = "refs/heads/";
   const headRef = process.env.GITHUB_HEAD_REF;
   let branch = headRef || github.context.ref;
-  if(branch && branch.indexOf(REFS_HEADS) === 0){
-    branch = branch.substring(REFS_HEADS.length)
+  if (branch && branch.indexOf(REFS_HEADS) === 0) {
+    branch = branch.substring(REFS_HEADS.length);
   }
   return branch;
 }
 
-function getSha(){
+function getSha() {
   const sha = github.context.sha;
-  if(!sha){
+  if (!sha) {
     throw Error(`Invalid sha ${sha}`);
   }
   return sha;
 }
 
 try {
-  const token = core.getInput('github-token', {required: true});
+  const token = core.getInput("github-token", { required: true });
 
-  const environment = core.getInput('environment', {required: true});
+  const environment = core.getInput("environment", { required: true });
   const branch = getBranch();
   const sha = getSha();
 
   const owner = github.context.repo.owner;
   const repo = github.context.repo.repo;
 
-  
   console.log(`environment ${environment}`);
   console.log(`branch ${branch}`);
   console.log(`sha ${sha}`);
   console.log(`owner ${owner}`);
   console.log(`repo ${repo}`);
 
-  const octokit = github.getOctokit(token)
+  const octokit = github.getOctokit(token);
 
   octokit.rest.repos.createDeployment({
     owner,
@@ -46,11 +45,11 @@ try {
     environment,
     auto_merge: false,
     payload: {
-      branch
+      branch,
     },
-  })
+  });
 
-  console.log("Github Deployment created")
+  console.log("Github Deployment created");
 } catch (error) {
   core.setFailed(`Error: Failed to create deployment: ${error.message}`);
 }
